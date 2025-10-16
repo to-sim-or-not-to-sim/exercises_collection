9edd4f247d2bc25795891a1eb5f079dc6f340251
to-sim-or-not-to-sim <sfeb04@gmail.com>
2025-10-16 12:03:01 +0200

    Add files via upload

--- MyLinearAlgebra.jl
+++ MyLinearAlgebra.jl
@@ -1,5 +1,6 @@
 module MyLinearAlgebra
 
+"""Returns the matrix with the elements of a vector on its diagonal: diagm(diagonal)."""
 function diagm(diagonal::Vector{T} where T<:Number)
     n=length(diagonal)
     A=zeros(Float64,n,n)
@@ -9,6 +10,7 @@ function diagm(diagonal::Vector{T} where T<:Number)
     return A
 end
 
+"""Returns x where Ax=b, where A is a lower triangular matrix and b is a vector."""
 function lower_triangular_matrix_solver(A::AbstractMatrix{T} where T<:Number,b::Vector{T} where T<:Number)
     n=length(b)
     x=zeros(Float64,n)
@@ -22,6 +24,7 @@ function lower_triangular_matrix_solver(A::AbstractMatrix{T} where T<:Number,b::
     return x
 end
 
+"""Returns x where Ax=b, where A is a upper triangular matrix and b is a vector."""
 function upper_triangular_matrix_solver(A::AbstractMatrix{T} where T<:Number,b::Vector{T} where T<:Number)
     n=length(b)
     x=zeros(Float64,n)
@@ -35,6 +38,7 @@ function upper_triangular_matrix_solver(A::AbstractMatrix{T} where T<:Number,b::
     return x
 end   
 
+"""Returns x where Ax=b, where A is a lower triangular matrix and b is a matrix."""
 function lower_triangular_matrix_solver(A::AbstractMatrix{T} where T<:Number,b::AbstractMatrix{T} where T<:Number)
     p,n=size(b)
     x=zeros(Float64,n,p)
@@ -50,6 +54,7 @@ function lower_triangular_matrix_solver(A::AbstractMatrix{T} where T<:Number,b::
     return x
 end
 
+"""Returns x where Ax=b, where A is a upper triangular matrix and b is a matrix."""
 function upper_triangular_matrix_solver(A::AbstractMatrix{T} where T<:Number,b::AbstractMatrix{T} where T<:Number)
     p,n=size(b)
     x=zeros(Float64,n,p)
@@ -65,6 +70,7 @@ function upper_triangular_matrix_solver(A::AbstractMatrix{T} where T<:Number,b::
     return x
 end  
 
+"""Returns L and U where LU=A with L lower triangular matrix and U upper triangular matrix."""
 function LU_decomposition(A::AbstractMatrix{T} where T<:Number)
     n,m=size(A)
     L=diagm(ones(n))
@@ -90,6 +96,7 @@ function display_LU(A::AbstractMatrix{T} where T<:Number)
     display(A-L*U)
 end
 
+"""Returns x where Ax=b using LU=A, where A is a matrix and b a vector."""
 function matrix_solver_LU(A::AbstractMatrix{T} where T<:Number,b::Vector{T} where T<:Number)
     L,U=LU_decomposition(A)
     z=lower_triangular_matrix_solver(L,b)
@@ -97,6 +104,7 @@ function matrix_solver_LU(A::AbstractMatrix{T} where T<:Number,b::Vector{T} wher
     return x
 end
 
+"""Returns x where Ax=b using LU=A, where A is a matrix and b a matrix."""
 function matrix_solver_LU(A::AbstractMatrix{T} where T<:Number,B::AbstractMatrix{T} where T<:Number)
     L,U=LU_decomposition(A)
     n,m=size(B)
@@ -120,6 +128,12 @@ function display_LU_solver(A::AbstractMatrix{T} where T<:Number,b::Vector{T} whe
     display(x)
 end
 
+"""Returns a 3x3 translation matrix T:\n
+\t\t  1 0 x \n
+T(x,y)= 0 1 y \n
+\t\t  0 0 1 \n
+translation_matrix(x,y).
+"""
 function translation_matrix(x::Number,y::Number)
     T=diagm(ones(3))
     T[1,3]=x
@@ -127,6 +141,12 @@ function translation_matrix(x::Number,y::Number)
     return T
 end
 
+"""Returns a 3x3 rotation matrix R:\n
+\t\t cos(a) sin(a) 0 \n
+R(a)=  -sin(a) cos(a) 0 \n
+\t\t      0      0 1 \n
+rotation_matrix(theta).
+"""
 function rotation_matrix(theta::Number)
     c=cos(theta)
     s=sin(theta)
@@ -136,7 +156,7 @@ function rotation_matrix(theta::Number)
     return R
 end
 
-
+"""Returns the determinant of a matrix A applying LU decomposition."""
 function det_LU(A::AbstractMatrix{T} where T<:Number)
     L,U=LU_decomposition(A)
     n,m=size(U)
@@ -147,10 +167,98 @@ function det_LU(A::AbstractMatrix{T} where T<:Number)
     return det
 end
 
-function inverse(A::AbstractMatrix{T} where T<:Number)
+"""Returns the inverse of a matrix A applying LU decomposition and solving the system: AX=I."""
+function inverse_LU(A::AbstractMatrix{T} where T<:Number)
     n,m=size(A)
     identity=diagm(ones(n))
     inverted=matrix_solver_LU(A,identity)
+    return inverted
+end
+
+"""Returns P, L and U where LU=PA with L lower triangular matrix, U upper triangular matrix and P permutation matrix."""
+function PLU_decomposition(A::AbstractMatrix{T} where T<:Number)
+    n,m=size(A)
+    L=diagm(ones(n))
+    P=diagm(ones(n))
+    U=zeros(Float64,n,n)
+    s=0
+    indexes=zeros(Int64,n)
+    exp_indexes=collect(1:n)
+    for i in 1:n
+        #---Permutations count---------------------#
+        indexes[i]=argmax(abs.(A[:,i]))            #
+        if exp_indexes[i]!=indexes[i]              #
+            s+=1                                   #
+            exp_index=exp_indexes[i]               #
+            exp_indexes[i]=indexes[i]              #
+            k=i+1                                  #
+            while exp_indexes[k]!=indexes[i]       #
+                k+=1                               #
+            end                                    #
+            exp_indexes[k]=exp_index               #
+        end                                        #
+        #------------------------------------------#
+        U[i,:]=A[indexes[i],:]
+        L[:,i]=A[:,i]/U[i,i]
+        A=A-L[:,i]*U[i,:]'
+    end
+    P=P[indexes,:]
+    L=L[indexes,:]
+    return P, L, U, s
+end 
+
+function display_PLU(A::AbstractMatrix{T} where T<:Number)
+    println("Matrix A:")
+    display(A)
+    P,L,U,s=PLU_decomposition(A)
+    println("Matrix P:")
+    display(P)
+    println("Matrix L:")
+    display(L)
+    println("Matrix U:")
+    display(U)
+    println("Matrix P*A-L*U:")
+    display(P*A-L*U)
+end
+
+"""Returns x where Ax=b using LU=PA, where A is a matrix and b a vector."""
+function matrix_solver_PLU(A::AbstractMatrix{T} where T<:Number,b::Vector{T} where T<:Number)
+    P,L,U,s=PLU_decomposition(A)
+    z=lower_triangular_matrix_solver(L,P*b)
+    x=upper_triangular_matrix_solver(U,z)
+    return x
+end
+
+"""Returns x where Ax=b using LU=PA, where A is a matrix and b a matrix."""
+function matrix_solver_PLU(A::AbstractMatrix{T} where T<:Number,B::AbstractMatrix{T} where T<:Number)
+    P,L,U,s=PLU_decomposition(A)
+    n,m=size(B)
+    X=zeros(Float64,n,m)
+    for i in 1:m
+        b=B[:,i]
+        z=lower_triangular_matrix_solver(L,P*b)
+        x=upper_triangular_matrix_solver(U,z)
+        X[:,i]=x
+    end
+    return X
+end
+
+"""Returns the determinant of a matrix A applying LU decomposition and the permutation matrix P."""
+function det_PLU(A::AbstractMatrix{T} where T<:Number)
+    P,L,U,s=PLU_decomposition(A)
+    n,m=size(U)
+    det=1
+    for i in 1:n
+        det*=U[i,i]
+    end
+    return det*((-1)^s)
+end
+
+"""Returns the inverse of a matrix A solving the system: AX=I, applying LU decomposition and the permutation matrix P."""
+function inverse_PLU(A::AbstractMatrix{T} where T<:Number)
+    n,m=size(A)
+    identity=diagm(ones(n))
+    inverted=matrix_solver_PLU(A,identity)
     return inverted
 end
 
