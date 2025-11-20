bafe050efe01d42ec1384ae4124721a8de5280ea
to-sim-or-not-to-sim <sfeb04@gmail.com>
2025-11-20 08:36:40 +0100

    Add comments to functions

--- MyInterpolation.jl
+++ MyInterpolation.jl
@@ -85,6 +85,7 @@ function equi_interpolation(x::Number,a::Number,b::Number,n::Int64,f::Function)
     return weight_mean(ys,weights)
 end
 
+"""Returns chebyshev nodes and the lambdas for the interpolation."""
 function chebyshev_nodes(n::Int64;a::Number=-1,b::Number=1,kind::Int64=2)
     x=zeros(Float64,n)
     lambda=zeros(Float64,n)
@@ -112,6 +113,7 @@ function chebyshev_nodes(n::Int64;a::Number=-1,b::Number=1,kind::Int64=2)
     return x,lambda
 end
 
+"""Computes the value of the interpolation for a function described by the points ('xs','ys') in a point 'x', when 'xs' are chebyshev nodes (both first and second kind, using "kind=1" or "kind=2") between a and b, using Lagrange Waring interpolation."""
 function chebyshev_interpolation(x::Number,f::Function,n::Int64;a::Number=-1,b::Number=1,kind::Int64=2)
     xs,lambda=chebyshev_nodes(n,a=a,b=b,kind=kind)
     ys=f.(xs)
@@ -126,6 +128,7 @@ function chebyshev_interpolation(x::Number,f::Function,n::Int64;a::Number=-1,b::
     return weight_mean(ys,weights)
 end
 
+"""Returns the inf-norm of two sets of data."""
 function inf_norm(y::Vector{T},y_fit::Vector{T}) where T<:Number
     len=length(y)
     if len!=length(y_fit)
@@ -135,10 +138,12 @@ function inf_norm(y::Vector{T},y_fit::Vector{T}) where T<:Number
     return maximum(abs.(y.-y_fit))
 end
 
+"""This function expand the chebyshev nodes in [-1,1] on the real axis using x'=2x/(1-x^2)."""
 function chebyshev2real(x::Vector{T}) where T<:Number
 	return @. 2*x/(1-x^2)
 end
 
+"""Computes the value of the interpolation for a function described by the points ('xs','ys') in a point 'x' on the real axis using Lagrange Waring interpolation and chebyshev nodes expanded using "chepyshev2real()" method."""
 function real_line_interpolation(z::Number,f::Function,n::Int64)
 	xs,lambda=chebyshev_nodes(n,kind=1)
 	zs=chebyshev2real(xs)
@@ -154,6 +159,7 @@ function real_line_interpolation(z::Number,f::Function,n::Int64)
     return weight_mean(ys,weights)
 end
 
+"""Computes trigonometric interpolation between a and b with N points."""
 function trigonometric_interpolation(x::Number,f::Function,N::Int64;a::Number=-1,b::Number=1)
     j=linspace(0,N-1,N)
     xs=@. a+j*(b-a)/N
