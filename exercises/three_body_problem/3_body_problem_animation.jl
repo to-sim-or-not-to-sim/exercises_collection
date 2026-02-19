388121dca4b90f213a2468a64a271a664553be59
to-sim-or-not-to-sim <sfeb04@gmail.com>
2026-02-19 14:58:44 +0100

    Add files via upload

--- exercises/three_body_problem/3_body_problem_animation.jl
+++ exercises/three_body_problem/3_body_problem_animation.jl
@@ -1,6 +1,6 @@
 using JSON,Plots
 pyplot()
-data=JSON.parsefile("3_body_problem.json")
+data=JSON.parsefile("./fisica computazionale/0/3bp_1_unbound/3_body_problem.json")
 x1=data["x1"]
 y1=data["y1"]
 x2=data["x2"]
@@ -10,15 +10,15 @@ y3=data["y3"]
 tis=data["t"]
 
 len=length(tis)
-start=1
-anim=@animate for i in 51:len
+anim=@animate for i in 1:10:len
     t_round=round(tis[i],digits=2)
     scatter([x1[i]],[y1[i]],color=:deepskyblue,title="t=$t_round",titlefontsize=15,legend=false)
     scatter!([x2[i]],[y2[i]],color=:crimson,legend=false)
     scatter!([x3[i]],[y3[i]],color=:goldenrod,legend=false)
-    plot!(x1[start:i],y1[start:i],color=:deepskyblue,legend=false)
-    plot!(x2[start:i],y2[start:i],color=:crimson,legend=false)
-    plot!(x3[start:i],y3[start:i],color=:goldenrod,legend=false)
-    start+=1
+    x=collect(1:i)
+    alpha_val=@. 1/(1+exp(-(x-i+50)/25))
+    plot!(x1[1:i],y1[1:i],alpha=alpha_val,color=:deepskyblue,legend=false)
+    plot!(x2[1:i],y2[1:i],alpha=alpha_val,color=:crimson,legend=false)
+    plot!(x3[1:i],y3[1:i],alpha=alpha_val,color=:goldenrod,legend=false)
 end
 gif(anim,"three_body_problem.gif",fps=30)
